f9ce8f9ebc92cc4b99c28c07b8f59d3532a0d516
Dan Greene <dsgreene713@gmail.com>
2022-10-10 10:13:44 -0400

    clean up files

--- eks/locals.tf
+++
@@ -1,9 +0,0 @@
-locals {
-  name            = "ex-${replace(basename(path.cwd), "_", "-")}"
-  cluster_version = "1.22"
-  region          = "us-east-1"
-
-  tags = {
-    Example = local.name
-  }
-}
